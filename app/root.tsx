ae82bf5548c2ffcade1f96bfd32c33b3b2cc584c
YoonHaeMin <yunhatmi@gachon.ac.kr>
2024-12-10 17:41:28 +0900

    feat: fsd 구조 확립

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,8 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+import "/app/styles/tailwind.css";
+import { NextUIProvider } from "@nextui-org/react";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -32,9 +33,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+        <NextUIProvider>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </NextUIProvider>
       </body>
     </html>
   );
---
+++ app/routes/lottery645/index.tsx
@@ -0,0 +1,14 @@
+import { Card, CardBody, CardHeader } from "@nextui-org/card";
+
+const Lottery645Page = () => {
+  return (
+    <div>
+      <Card>
+        <CardHeader>6/45로또 데이터 조회</CardHeader>
+        <CardBody></CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Lottery645Page;
